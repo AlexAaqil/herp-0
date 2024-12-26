c7e20ae6d74e9e1a4f808ae900cfba574f024271
alexaaqil <alexaaqil.se@gmail.com>
2024-12-26 16:17:45 +0300

    Login: modified the login page to make it possible to have 3 login pages for the staff, students and parents.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,45 @@
 <x-general-layout class="Authentication">
-    <section class="container Login">
+    <section class="Login">
+        <div class="container">
+            <div class="portals">
+                <p class="portal active">
+                    <a href="{{ route('login') }}">Staff</a>
+                </p>
+                <p class="portal">
+                    <a href="#">Student</a>
+                </p>
+                <p class="portal">
+                    <a href="#">Parent</a>
+                </p>
+            </div>
+
+            <div class="login_form custom_form">
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+
+                    <div class="input_group">
+                        <label for="email">Email Address</label>
+                        <input type="email" name="email" id="email" placeholder="Email Address"
+                            value="{{ old('email') }}">
+                        <span class="inline_alert">{{ $errors->first('email') }}</span>
+                    </div>
+
+                    <div class="input_group">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password" placeholder="Password"
+                            value="{{ old('password') }}">
+                    </div>
+
+                    <p>
+                        <a href="{{ route('password.request') }}">Forgot your password?</a>
+                    </p>
+
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+        </div>
+    </section>
+    {{-- <section class="container Login">
         <div class="custom_form">
             <form method="POST" action="{{ route('login') }}">
                 @csrf
@@ -24,5 +64,5 @@
         
             <p>Don't have an account? <a href="{{ route('register') }}">Signup</a></p>
         </div>
-    </section>
+    </section> --}}
 </x-general-layout>
