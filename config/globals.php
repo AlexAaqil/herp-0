b73a91bba955932972b05dbfef2a96bb67d43c8b
alexaaqil <alexaaqil.se@gmail.com>
2024-12-28 10:10:04 +0300

    Config/globals: added the default gender for students to make it easier to autofill the gender.

--- config/globals.php
+++ config/globals.php
@@ -4,6 +4,7 @@ return [
     'app_short_name' => 'HSMS',
     'app_full_name' => 'High School Management System',
     'app_slogan' => 'Cool app',
+    'students_gender' => 'M',
     'phone_number' => '+254 746 055 487',
     'whatsapp_number' => '254746055487',
     'email' => 'alexaaqil.se@gmail.com',
