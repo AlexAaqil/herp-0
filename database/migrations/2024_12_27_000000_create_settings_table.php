0b66689ae440c1787a8c083588869d83a995f8db
alexaaqil <alexaaqil.se@gmail.com>
2025-01-05 18:24:34 +0300

    Settings: added the fields for the settings table.

--- database/migrations/2024_12_27_000000_create_settings_table.php
+++ database/migrations/2024_12_27_000000_create_settings_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
+            $table->string('type');
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
