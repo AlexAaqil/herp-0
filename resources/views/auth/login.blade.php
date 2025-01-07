b35d14d3c057ac2b6600ee8995f1e0827f1225dd
alexaaqil <alexaaqil.se@gmail.com>
2025-01-07 05:28:53 +0300

    Authentication: re-styles the basic authentication pages.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,65 +1,35 @@
 <x-general-layout class="Authentication">
     <section class="Login">
-        <div class="container">
-            <div class="portals">
-                <p class="portal">
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+
+            <div class="header portals">
+                <p class="active">
                     <a href="{{ route('login') }}">Staff</a>
                 </p>
-                <p class="portal active">
+                <p>
                     <a href="{{ route('student-login') }}">Student / Parent</a>
                 </p>
             </div>
 
-            <div class="login_form custom_form">
-                <form method="POST" action="{{ route('login') }}">
-                    @csrf
-
-                    <div class="input_group">
-                        <label for="email">Email Address</label>
-                        <input type="email" name="email" id="email" placeholder="Email Address"
-                            value="{{ old('email') }}">
-                        <span class="inline_alert">{{ $errors->first('email') }}</span>
-                    </div>
-
-                    <div class="input_group">
-                        <label for="password">Password</label>
-                        <input type="password" name="password" id="password" placeholder="Password"
-                            value="{{ old('password') }}">
-                    </div>
-
-                    <p>
-                        <a href="{{ route('password.request') }}">Forgot your password?</a>
-                    </p>
-
-                    <button type="submit">Login</button>
-                </form>
+            <div class="input_group">
+                <label for="email">Email Address</label>
+                <input type="email" name="email" id="email" placeholder="Email Address"
+                    value="{{ old('email') }}">
+                <span class="inline_alert">{{ $errors->first('email') }}</span>
             </div>
-        </div>
+
+            <div class="input_group">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" placeholder="Password"
+                    value="{{ old('password') }}">
+            </div>
+
+            <p>
+                <a href="{{ route('password.request') }}">Forgot your password?</a>
+            </p>
+
+            <button type="submit">Login</button>
+        </form>
     </section>
-    {{-- <section class="container Login">
-        <div class="custom_form">
-            <form method="POST" action="{{ route('login') }}">
-                @csrf
-        
-                <div class="input_group">
-                    <label for="email">Email Address</label>
-                    <input type="email" name="email" id="email" placeholder="Email Address" value="{{ old('email') }}">
-                    <span class="inline_alert">{{ $errors->first('email') }}</span>
-                </div>
-        
-                <div class="input_group">
-                    <label for="password">Password</label>
-                    <input type="password" name="password" id="password" placeholder="Password" value="{{ old('password') }}">
-                </div>
-        
-                <p>
-                    <a href="{{ route('password.request') }}">Forgot your password?</a>
-                </p>
-        
-                <button type="submit">Login</button>
-            </form>
-        
-            <p>Don't have an account? <a href="{{ route('register') }}">Signup</a></p>
-        </div>
-    </section> --}}
 </x-general-layout>
--- resources/views/auth/student_login.blade.php
+++ resources/views/auth/student_login.blade.php
@@ -1,39 +1,35 @@
 <x-general-layout class="Authentication">
     <section class="Login">
-        <div class="container">
-            <div class="portals">
-                <p class="portal active">
+        <form method="POST" action="{{ route('login-student') }}">
+            @csrf
+
+            <div class="header portals">
+                <p>
                     <a href="{{ route('login') }}">Staff</a>
                 </p>
-                <p class="portal">
+                <p class="active">
                     <a href="{{ route('student-login') }}">Student / Parent</a>
                 </p>
             </div>
 
-            <div class="login_form custom_form">
-                <form method="POST" action="{{ route('login-student') }}">
-                    @csrf
-
-                    <div class="input_group">
-                        <label for="registration_number">Registration Number</label>
-                        <input type="text" name="registration_number" id="registration_number" placeholder="Registration Number"
-                            value="{{ old('registration_number') }}">
-                        <span class="inline_alert">{{ $errors->first('registration_number') }}</span>
-                    </div>
-
-                    {{-- <div class="input_group">
-                        <label for="password">Password</label>
-                        <input type="password" name="password" id="password" placeholder="Password"
-                            value="{{ old('password') }}">
-                    </div>
-
-                    <p>
-                        <a href="{{ route('password.request') }}">Forgot your password?</a>
-                    </p> --}}
-
-                    <button type="submit">Login</button>
-                </form>
+            <div class="input_group">
+                <label for="registration_number">Registration Number</label>
+                <input type="text" name="registration_number" id="registration_number"
+                    placeholder="Registration Number" value="{{ old('registration_number') }}">
+                <span class="inline_alert">{{ $errors->first('registration_number') }}</span>
             </div>
-        </div>
+
+            {{-- <div class="input_group">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" placeholder="Password"
+                    value="{{ old('password') }}">
+            </div>
+
+            <p>
+                <a href="{{ route('password.request') }}">Forgot your password?</a>
+            </p> --}}
+
+            <button type="submit">Login</button>
+        </form>
     </section>
 </x-general-layout>
