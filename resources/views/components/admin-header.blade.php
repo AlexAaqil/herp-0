f01e3183cbe6c936527062e651ff180fe9d2d234
alexaaqil <alexaaqil.se@gmail.com>
2025-01-12 04:04:03 +0300

    Admin Header: modified the admin header span so that the total_count can be optional.

--- resources/views/components/admin-header.blade.php
+++ resources/views/components/admin-header.blade.php
@@ -1,7 +1,12 @@
 @props(['header_title', 'total_count', 'route'])
 
 <div class="header">
-    <p>{{ $header_title }} <span>({{ $total_count }})</span></p>
+    <p>
+        {{ $header_title }} 
+        @if(isset($total_count))
+            <span>({{ $total_count }})</span>
+        @endif
+    </p>
     <input type="text" name="search" id="myInput" onkeyup="searchFunction()" placeholder="Search">
     @if(isset($route))
         <div class="header_btn">
